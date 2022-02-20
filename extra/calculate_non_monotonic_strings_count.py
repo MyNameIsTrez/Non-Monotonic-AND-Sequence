a8c541c290ed3ca6296aba66e1b68f5a7de1afe4
MyNameIsTrez <welfje@gmail.com>
2022-02-20 23:58:47 +0100

    Add get_non_monotonic_strings_dict() and calculate_non_monotonic_strings_count() in the extras directory

---
+++ extra/calculate_non_monotonic_strings_count.py
@@ -0,0 +1,17 @@
+def calculate_non_monotonic_strings_count():
+	n = 0
+
+	for start in range(1, 10):
+			for i in range(start, 10):
+				n += i
+
+	return n
+
+
+def main():
+	non_monotonic_strings_count = calculate_non_monotonic_strings_count()
+	print(non_monotonic_strings_count)
+
+
+if __name__ == "__main__":
+	main()
---
+++ extra/get_non_monotonic_strings_dict.py
@@ -0,0 +1,27 @@
+import pprint
+
+
+def get_non_monotonic_strings_dict():
+	non_monotonic_strings_dict = {}
+
+	for i in range(10):
+		for j in range(10):
+			for k in range(10):
+				if (i < j > k):
+					if i not in non_monotonic_strings_dict:
+						non_monotonic_strings_dict[i] = {}
+					if j not in non_monotonic_strings_dict[i]:
+						non_monotonic_strings_dict[i][j] = {}
+					non_monotonic_strings_dict[i][j][k] = f"{i}{j}{k}"
+
+	return non_monotonic_strings_dict
+
+
+def main():
+	non_monotonic_strings_dict = get_non_monotonic_strings_dict()
+	# This can be piped to a file for easier viewing.
+	pprint.pprint(non_monotonic_strings_dict, width=160)
+
+
+if __name__ == "__main__":
+	main()
