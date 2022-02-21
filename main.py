65dc8549db935084a8c597bf8478ce8c8bdf37ec
MyNameIsTrez <welfje@gmail.com>
2022-02-21 01:46:28 +0100

    Replace non_monotonic_strings list with smarter slope math

--- main.py
+++ main.py
@@ -2,33 +2,19 @@ import operator
 
 
 def main(binary_fn):
-	non_monotonic_strings = get_non_monotonic_strings()
-
 	print(f"binary function: {binary_fn}\n")
 
 	right = 0
 	while True:
 		sequence = get_sequence(right, binary_fn)
 
-		if is_monotonic(sequence, non_monotonic_strings):
+		if is_monotonic(sequence):
 			print(f"right: {right}, monotonic sequence: {sequence}\n")
 
 		right += 1
 
-		if right % 1000 == 0:
-			print(right)
-
-
-def get_non_monotonic_strings():
-	non_monotonic_strings = []
-
-	for i in range(10):
-		for j in range(10):
-			for k in range(10):
-				if (i < j > k):
-					non_monotonic_strings.append(f"{i}{j}{k}")
-
-	return non_monotonic_strings
+		# if right % 1000 == 0:
+		# 	print(right)
 
 
 def get_sequence(right, binary_fn):
@@ -59,11 +45,35 @@ def get_most_significant_bit_index(n):
 	return i
 
 
-def is_monotonic(sequence, non_monotonic_strings):
-	for non_monotonic_string in non_monotonic_strings:
-		if non_monotonic_string in sequence:
+def is_monotonic(sequence):
+	sequence_len = len(sequence)
+
+	if sequence_len <= 1:
+		return True
+
+	start_slope_direction = None
+
+	for i in range(0, sequence_len - 1):
+		slope_direction = get_slope_direction(sequence[i], sequence[i + 1])
+
+		if start_slope_direction == None and slope_direction != 0:
+			start_slope_direction = slope_direction
+		elif slope_direction == -1 and start_slope_direction == 1:
 			return False
+		elif slope_direction == 1 and start_slope_direction == -1:
+			return False
+
 	return True
+
+
+def get_slope_direction(a, b):
+	n = int(b) - int(a)
+	if n > 0:
+		return 1
+	elif n == 0:
+		return 0
+	else:
+		return -1
 
 
 if __name__ == "__main__":
